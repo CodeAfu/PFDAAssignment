47a04b1fc8badb721e7eb4600a90d5b40f3e6271
CodeAfu <ibrahim.afzal1999@gmail.com>
2024-05-26 15:04:06 +0800

    Begin work

--- analysis/ibrahim.R
+++ analysis/ibrahim.R
@@ -2,7 +2,17 @@
 library(tidyverse)
 library(ggplot2)
 
-
 df <- read_csv("./data/clean_credit_score.csv")
 
-View(df)
+plot_one <- function() {
+  plot <- ggplot(df, aes(x = outstanding_debt)) +
+    geom_histogram(binwidth = 300, fill = "blue", color = "black", alpha = 0.5) +
+    labs(title = "Distribution of Outstanding Debt", x = "Outstanding Debt", y = "Count")
+
+
+  ggsave("./plots/test.png", plot = plot, width = 16, height = 10)
+}
+
+plot_one()
+
+
