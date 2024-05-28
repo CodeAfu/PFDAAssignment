80851aa80c54359abcc69b8dc27f5e99e5305738
CodeAfu <ibrahim.afzal1999@gmail.com>
2024-05-28 13:15:56 +0800

    Removed ibrahim's personal anaylsis files from the main branch

--- analysis/ibrahim.extras.R
+++
@@ -1,8 +0,0 @@
-
-# pair_plot <- function() {
-#   plot <- ggpairs(df, columns = c("outstanding_debt", "credit_score", "monthly_inhand_salary", "num_bank_accounts", "num_credit_card", "interest_rate"), aes(color = credit_score, alpha = 0.5))
-  
-#   ggsave("./plots/outstanding_debt/pair_plot.png", plot = plot, width = 30, height = 30)
-# }
-
-# pair_plot()
